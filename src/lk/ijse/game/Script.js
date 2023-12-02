ccd3bf12ebaa2ad93d70f4e0fa3e2a3bc3602090
dinithimendis <dinithimendis42@gmail.com>
2023-12-02 08:59:22 +0530

    adding js to start

--- src/lk/ijse/game/Script.js
+++ src/lk/ijse/game/Script.js
@@ -68,3 +68,15 @@ const generateGame = () => {
 
     selectors.board.replaceWith(parser.querySelector('.board'))
 }
+
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `time: ${state.totalTime} sec`
+    }, 1000)
+}
